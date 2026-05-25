13aea0cf1150afbd28f1c29d61ae0bdf044dcefd
cbnui <107230821+cbnui@users.noreply.github.com>
2026-05-25 10:24:13 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -59,8 +59,10 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-# FRP
-#PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
+# frpc
+PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
+# openlist
+PACKAGES="$PACKAGES luci-i18n-openlist-zh-cn"
 
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
