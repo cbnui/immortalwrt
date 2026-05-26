93aa7f00eb6b5719408103a0194a6532144bcaa3
cbnui <107230821+cbnui@users.noreply.github.com>
2026-05-26 15:24:53 +0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -159,6 +159,8 @@ uci set easytier.cfg01894b.ws_port='11011'
 uci set easytier.cfg01894b.wss_port='11012'
 uci set easytier.cfg01894b.desvice_name='tr3000'
 uci set easytier.cfg01894b.log='off'
+uci del easytier.cfg01894b.auto_config_interface
+uci del easytier.cfg01894b.auto_config_firewall
 uci set easytier.cfg01894b.et_forward='etfwlan etfwwan lanfwet wanfwet'
 uci commit easytier
 /etc/init.d/easytier restart
